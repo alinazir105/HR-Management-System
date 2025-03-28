9ea8bed051d85697dd4477a4bc844c080c810d1a
Amalark <amal.ar.khalili@gmail.com>
2025-03-28 21:59:22 +0500

    login page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 import Login from "./pages/Login";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
