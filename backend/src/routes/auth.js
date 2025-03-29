2653c4abcea3cf579897dad870e9b450477cb706
Rayyanalii <rayyanali2705@gmail.com>
2025-03-29 17:52:49 +0500

    Implemented authorization and protected routing for HR and employee dashboards

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pool from "../db.js";
 import bcrypt from "bcrypt";
+import { authMiddleware } from "./../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -21,7 +22,7 @@ router.post("/login", async (req, res) => {
     return res.status(400).json({ message: "Invalid Credentials" });
   }
 
-  req.session.user = {
+  req.session.data = {
     id: userData.id,
     role: userData.role,
   };
@@ -37,6 +38,10 @@ router.post("/logout", (req, res) => {
     res.clearCookie("connect.sid");
     res.json({ message: "Logged out successfully" });
   });
+});
+
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({ message: req.session.data });
 });
 
 export default router;
