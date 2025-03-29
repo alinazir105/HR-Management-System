d6f7f7c859f9d03d5f8dc6b19922932ff7356e76
Amalark <amal.ar.khalili@gmail.com>
2025-03-29 22:21:09 +0500

    Started HR Sidebar

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 export const authMiddleware = (req, res, next) => {
+  console.log("Session Data:", req.session);
+  console.log(req.session.data);
   if (!req.session.data) {
     return res.status(401).json({ message: "Unauthorized" });
   }
